8e8a35a43693d225e95e8ae4588f47822212f04a
matthewmamelak <49646724+matthewmamelak@users.noreply.github.com>
2022-01-17 12:21:29 -0500

    Add files via upload

---
+++ L5_HASH.c
@@ -0,0 +1,190 @@
+//LAB5_HASH.C - Lab 05 - Cramarossa, Alexandra and Mamelak, Matthew
+// Program for use with ELEC278 Lab 5.
+//
+// History:
+// 161106	HF	First release
+// 171102	DA	Minor tweaks
+// 201101	DA	Format changes
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "C://Users//acram//AppData//Local//Temp//DLL.h"
+
+
+// Definition of data structure for chained hashtable
+typedef struct HashTable {
+    pLinkedList	*a;			// point to array of pointers to linked lists
+    int			size;		// size of the array (not the table)
+    int			population;	// actual number of strings in table
+    int			collisions;	// count collisions detected on insert
+} HashTable, *pHashTable;
+
+
+pHashTable initHashTable (int s)
+{
+    pHashTable	pht = (pHashTable) malloc(sizeof(HashTable));
+    if (pht != NULL)	{
+        // Created space for hashtable descriptor
+        int		i;
+        pht->size = s;		// size is the size of the linked-list array
+        pht->collisions = 0;
+        pht->population = 0;
+        // Now set up the harray of linked list pointers for the chains
+        pht->a = malloc(sizeof(LinkedList) * s);
+        if (pht->a != NULL)	{
+            // Initialize memory for all the linked lists
+            for(i=0; i<s; i++)	pht->a[i] = init();
+        } else	{
+            // Didn't get needed memory - give up what we got
+            free (pht);
+            pht = NULL;
+        }
+    }
+    return pht;
+}//initHashTable()
+
+
+unsigned long int hashcode (char *x)
+// This function computes hash code for given string.  Note that hash code
+// is unbounded - it needs to be reduced modulo size of hash table (array)
+// used to store data. (You will see another function hash() below that
+// completes the index calculation.)
+{
+    unsigned long int	sum = 0;
+    int		i = 0;
+    int l = strlen(x);
+    while(x[i]!='\0'){
+        sum += (x[i] * (long)pow(31,l-1-i));
+        i++;
+    }
+    return sum;
+}//hashcode()
+
+
+int hash (char* x, int size)
+// Index calculation. Function hashcode() computes general hash for string;
+// this function turns hash into valid index, given size of array.
+{
+    return hashcode(x) % size;
+}//hash()
+
+
+void insert(HashTable *ht, char *x)
+// Insert new string into hashtable
+{
+    int key = hash(x, ht->size);
+    printf ("    Word: %s hash index: %d\n", x, key);
+    // Is the slot empty?
+    if (!isEmptyList(ht->a[key]))	ht->collisions++;
+    addFront(ht->a[key],x);
+    ht->population++;
+}//insert()
+
+
+int findInTable(HashTable *ht, char* x)
+{
+    int key = hash(x, ht->size);
+    return (find(ht->a[key],x));
+}
+
+float loadFactor(HashTable *ht) {
+//returns a float value of the load factor (population in hash table / size)
+    return (float) ht->population / ht->size;
+}
+
+void printTable(HashTable *ht)
+{
+    int i;
+    printf("----------\n");
+    for (i = 0; i < ht->size; i++)	{
+        printf("%d --> ", i);
+        printList(ht->a[i]);
+    }
+    printf("# of Collisions=%d # entries=%d\n", ht->collisions, ht->population);
+    printf("Load Factor %%=%.2f\n----------", loadFactor(ht));
+}
+
+#define	TABSIZ	42				// define size of table
+//42 was chosen as it had both a better ACL and LF than 41
+//Though 43 had a better LF, 42 did have a better ACL than 43
+//Since the lab deliverable specifies ACL, 42 was chosen
+
+void ACLprint(HashTable *ht){
+//sums all chains non-zero length in the hash table and divides by the # of non-zero
+//length chains
+
+    //assign variables to the size of the hashtable and sumChain to 0
+    int numChains_nonZero = ht->size;
+    int sumChain = 0;
+
+    for (int i = 0; i < (ht->size); i++)   {
+
+        if (ht->a[i]->size == 0){   //Checks to see if size of linked list is 0
+            numChains_nonZero--;    //If zero, remove one from chain
+        }
+
+        else {    //Otherwise add size of the linked list to the chainSum
+            sumChain += ht->a[i]->size;
+        }
+    }
+
+    //print out message showing the ACL
+    printf("\nThe Average Chain Length is: %.2f\n", (float)sumChain/numChains_nonZero);
+}
+
+
+
+int main()
+{
+    int i=0;
+    HashTable* h = initHashTable(TABSIZ+1);
+
+// Two versions of code to build data. The first generates random strings, the
+// second (used in the lab) reads words from a file.
+#if 0
+    // Generate a set of random strings (given the way this works, everyone
+	// gets the same set of random strings!) insert each into the hash table
+	// and then print the table.
+	for(i=0;i<TABSIZ;i++){
+		int x = rand()%5+1;
+		char *p = malloc(sizeof(char)*x);
+		int j=0;
+		for(j=0;j<x;j++)
+			p[j] = rand()%26 + 97;
+		p[j]='\0';
+		insert(h,p);
+	}
+#else
+
+    FILE	*fp;
+    char	*p;
+    char	buff [132];
+    fp = fopen ("C://Users//acram//AppData//Local//Temp//WORDLIST.TXT", "r");
+    while (!feof (fp))	{
+        fgets (buff, 128, fp);
+        p = buff;
+        while (*p)	 {
+            if (*p <= ' ')	{
+                *p = '\0';
+                break;
+            }
+            p++;
+        }
+        printf ("Word inserted: %s\n", buff);
+        insert (h, buff);
+        printTable (h);
+        printf ("Press return key to add another number to table:");
+        fflush (stdout);
+        getchar();
+    }
+#endif
+    printTable(h);
+
+    ACLprint(h);
+
+    return 0;
+}
+
